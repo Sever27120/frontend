e5d717b209c6f3fb32f6a6b161d7031b080f947b
Séverine philippe <severine.philippe27@gmail.com>
2023-04-07 11:51:12 +0200

    gestion info

--- Json/JS - 19 - Projet Gestion ouvrages informatiques/JS - 19 - Projet Gestion ouvrages informatiques/script.js
+++ Json/JS - 19 - Projet Gestion ouvrages informatiques/JS - 19 - Projet Gestion ouvrages informatiques/script.js
@@ -12,6 +12,11 @@ var authorsList = new Array();
 
 var categoriesList = new Array();
 
+// on lance le chargement
+const loader=document.querySelector("loader");
+
+
+
 window.addEventListener("load", jasonOnLoad());
 // On lance le filtre par auteur quand on change sur la liste déroulante
 document.getElementById("listAuthors").addEventListener("change", function () {
@@ -22,6 +27,8 @@ document.getElementById("listCategories").addEventListener("change", function ()
     ChargeByCategorie();
 });
 
+
+// fonction pour pouvoir mettre le json
 function jasonOnLoad() {
 
     fetch("books.json")
@@ -33,7 +40,7 @@ function jasonOnLoad() {
             createListe(booksData);
         });
 }
-
+//fonction création de nos liste déroulante
 function createListe(_data) {
     for (var x = 0; x < _data.length; x++) {
         var book = _data[x];
@@ -48,12 +55,12 @@ function createListe(_data) {
         }
 
         //on créer la liste des catégories
-       /* for (var z = 0; z < book.categories.length; z++)
+        for (var z = 0; z < book.categories.length; z++)
             var categorie = book.categories[z];
 
         if (categoriesList.indexOf(categorie) == -1) {
             categoriesList.push(categorie);
-        }*/// pas demander dans le cahier des charges.
+        }
 
 
     }
@@ -77,67 +84,93 @@ function createListe(_data) {
     }
     showBooks(booksList);
 }
-
+// pour afficher les images
 function showBooks(_booksList) {
-    document.getElementById("booksList").innerHTML = "";
-
+    document.getElementById("booksList").innerHTML ="";
+// création des cartes pour chaque livres 
     for (var i = 0; i < _booksList.length; i++) {
         var bookCard = document.createElement("div");
         bookCard.setAttribute("class", "card mb-4");
-
+// pour afficher les images
         if (_booksList[i].thumbnailUrl == undefined ||
             _booksList[i].thumbnailUrl == null) {
-            _booksList[i].thumbnailUrl= 
+            _booksList[i].thumbnailUrl =
                 "https://p1.storage.canalblog.com/14/48/1145642/91330992_o.png";
         }
-
+        // conditions pour pouvoir afficher  les titres avec conditions de 20 caractères sinon ect
         var titre;
         if (_booksList[i].title.length > 20) {
-            titre = _booksList[i].title.substring(0,20) + "(..)"
+            titre = _booksList[i].title.substring(0, 20) + "(..)"
 
         }
         else {
             titre = _booksList[i].title;
         }
 
-       
+        // conditions pour avec les nombre de pages
+        
+        var nbrpage
+
+        if (_booksList[i].pageCount == undefined ||
+            _booksList[i].pageCount == 0) 
+            
+            {
+               nbrpage="le nombre de page n'est pas indiquer";
+            }
+
+            else{
+                nbrpage =_booksList[i].pageCount;
+            }
+            
+        // conditions  afficher isbn       
+        var ISBN;
+
+
+        ISBN = _booksList[i].isbn;
+
+        // conditions pour  les description inférieur à 20 caractéreur sinon ect     
         var description;
         var descriptionshort;
 
         if
             (_booksList[i].shortDescription == undefined ||
-            _booksList[i].shortDescription == null) 
-            {
+            _booksList[i].shortDescription == null) {
             description = "";
             descriptionshort = "";
         }
         else {
             if (booksList[i].shortDescription.length > 20) {
 
-                descriptionshort = _booksList[i].shortDescription.substring(0,20) + "(..)";
+                descriptionshort = _booksList[i].shortDescription.substring(0, 20) + "(..)";
                 description = _booksList[i].shortDescription;
             }
             else {
                 descriptionshort = _booksList[i].shortDescription;
                 description = _booksList[i].shortDescription;
             }
+            // mettre la date publication en français comme lundi 9 avril 2009
 
             var datePubli;
             try {
                 datePubli = new Date(_booksList[i].publishedDate.dt_txt)
                     .toLocaleDateString("fr-FR", options);
-        
+
             } catch (error) {
                 datePubli = "Pas date de publication";
             }
-        
+// afficher sur le html donc sur la page web vue par l'utilisatuer 
             bookCard.innerHTML = "<img src='" + _booksList[i].thumbnailUrl + "'/>" +
-              
+                // afficher pas plus de 20 caractère du titre
+                //afficher dans une infobulle le titre des mots qui manque
                 "<h1 class='bookTitle'><span class='infobulle' title='" +
                 _booksList[i].title + "'>" +
                 titre + "</span></h1>" +
-                //pas demander dans le cahier des charges comme c'est pour afficher que les 20 caractères.
+                // AFfficher isbn
+                "<h2>"+ ISBN + "</h2>" +
+                // afficher nombre de pages
+                "<h3>"+ nbrpage + "</h3>" +
                 "<h4>" + datePubli + "</h4>";
+// afficher dans une bulle le reste des descriptions pour voir la suite indiquer avec (..)
             if (description != "") {
                 bookCard.innerHTML += "<h4> <span class='infobulle' title='" +
                     _booksList[i].shortDescription + "'>" + description + "</span></h4>";
@@ -147,12 +180,8 @@ function showBooks(_booksList) {
 
     }
 
-
-  
-   
-
 }
-
+//fonction pour afficher les livres par auteurs avec la liste déroulante
 function ChargeByAuthor() {
     var e = document.getElementById("listAuthors");
     var strAuthors = e.options[e.selectedIndex].innerText;
@@ -171,9 +200,10 @@ function ChargeByAuthor() {
     }
     booksByAuthorsList.sort();
     showBooks(booksByAuthorsList);
+    Time(booksByAuthorsList);
 
 }
-
+// fonction pour afficher les livres par cathégories avec la liste déroulante
 function ChargeByCategorie() {
     var e = document.getElementById("listCategories");
     var strCategories = e.options[e.selectedIndex].innerText;
@@ -184,13 +214,23 @@ function ChargeByCategorie() {
     }
     else {
         for (var y = 0; y < booksList.length; y++) {
-            varbookByCategorie = booksList[y];
-            if (bookCategorie.categories.indexOf(strCategories) != -1) {
+            var bookByCategorie = booksList[y];
+            if (bookByCategorie.categories.indexOf(strCategories) != -1) {
                 booksByCategoriesList.push(bookByCategorie);
             }
         }
     }
     booksByCategoriesList.sort();
     showBooks(booksByCategoriesList);
+    Time(booksByCategoriesList);
 
 }
+
+/*function Time(){
+
+document.getElementsByClassName("loader").setTimeout(() => {
+    alert("Aucune recherche à votre demande")
+    
+}, 4000);
+} cette fonction ne fonctionne pas pour indiquer que dès 
+le dépassemnt de 4 s de chargement indiquer un message d'erreur que les info demander pas trouver*/
