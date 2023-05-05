7e3f5227ae7a6f4b4b0887c9e5bc7aeb31fc05df
Séverine philippe <severine.philippe27@gmail.com>
2023-05-05 14:09:29 +0200

    ecf 2 ratrapage

--- ECF2 Philippe Séverine/IMC/imc.js
+++ ECF2 Philippe Séverine/IMC/imc.js
@@ -1,46 +1,70 @@
-function calculIMC() {
 
-    const poids = prompt("Allez-y, vous pesez combien de kilos ?");
-    
-    if (poids == 0 && poids == null) {
-    
-    alert("Désoler mais je ne peux rien faire si vous ne me donnez pas votre poids !")
-    
-    } else if (poids > 0) {
-    
-    const taille = prompt("Et maintenant, sans vous mettre sur la pointe des pieds et lever les bras, vous mesurez combien de mètre (n'oubliez pas le point entre le mètre et les centimètres) ?");
-    
-    imc = poids / (taille * taille);
-    
-    if (imc <= 16.5) {
-    
-    alert("AHHHHHHH, vous êtes avec un IMC de " + imc + " ! \n Vous êtes en dénutrition ! \n IL FAUT MANGER !!!")
-    
-    } else if (imc > 16.5 && imc <= 18.5) {
-    
-    alert("Votre imc est de " + imc.toFixed(2) + " ! \n Vous êtes maigre et ça se voit ! MANGER !!!")
-    
-    } else if (imc > 18.5 && imc <= 25) {
-    
-    alert("Votre imc est de " + imc.toFixed(2) + " ! \n Vous êtes de corpulence normale, continuer comme ça !")
-    
-    } else if (imc > 25 && imc <= 30) {
-    
-    alert("Votre imc est de " + imc.toFixed(2) + " ! \n Vous êtes en surpoids...ça va,c'est que quelques kilos à perdre ou des centimètres à prendre !")
-    
-    } else if (imc > 30 && imc <= 35) {
-    
-    alert("Votre imc est de " + imc.toFixed(2) + " ! \n Vous êtes en obésité modéré...Allez courir un peu en pleine air pour vous revitaliser et manger ces étranges fruits verts qu'on appelle \" légume \" ! ")
-    
-    } else if (imc > 35 && imc <= 40) {
-    
-    alert("Votre imc est de " + imc.toFixed(2) + " ! \n Vous êtes en obésité sévère ET IL NE VOUS RESTE QU'UNE SEULE ÉTAPE AVANT D'ÊTRE EN OBÉSITÉ MORBIDE DONC VOUS VOUS REPRENEZ EN MAIN ET VOUS ALLEZ VOIR VOTRE MÉDECIN POUR SAVOIR QUOI FAIRE !")
-    
-    } else {
-    alert("Votre imc est de " + imc.toFixed(2) + " ! \n VOUS ÊTES EN OBÉSITÉ MORBIDE !!! C'EST UNE HONTE ET UNE TRISTESSE PACHYDERMIQUE,IL VA FALLOIR TRAVAILLER DUR POUR VOUS REMETTRE SUR PIED ET VOUS ALLEZ SOUFFRIR, AHHHHH OUI,POUR SÛR QUE VOUS ALLEZ SOUFFRIR !")
-    
+const calculateur= document.getElementsClassName("calculateur");
+const final=document.getElementsByClassName("final");
+
+function calculimc(){
+
+
+    const prenom = document.getElementById("prenom");
+    const poid = document.getElementById("poid");
+    const taille = document.getElementById("taille");
+
+    if (prenom.value || poid.value || taille.value) {
+        if (prenom.value && poid.value && taille.value) {
+            prenom.style.border = "1px solid red";
+            poid.style.border = "1px solid red";
+            taille.style.border = "1px solid red";
+            alert("Veuillez indiquer toute les informations nécessaire pour le calcul");
+
+        }
+        else if (prenom.value) {
+            prenom.style.border = "1px solid red";
+            alert("Veuillez indiquer votre prenom et nom");
+        }
+
+        else if (poid.value) {
+            poid.style.border = "1px solid red";
+            alert("Veuillez indiquer votre poid");
+        }
+
+        else {
+            taille.style.border = "1px solid red";
+            alert("Veuillez indiquer votre taille");
+        }
     }
-    
+    else {
+
+        let imc = poid.value / (taille.value * taille.value);
+        const resul = document.getElementById("final");
+
+        if ( imc <= 18.4) {
+
+            resul.innerHTML= (prenom +"' " +'<br>'+"Votre poid est de"+ " "+poid+ "<br>"+"Votre taille est de" +" "+taille+ "<br>" +"Votre imc est de "+" "+ imc + "<br>"+ "Vous êtes maigre");
+            resul.style.color="yellow";
+        } 
+        else if (imc > 18.5 && imc <= 24.9) {
+
+            resul.innerHTML= (prenom +"' " +'<br>'+"Votre poid est de"+ "' "+poid+ "<br>"+"Votre taille est de" +"' "+taille+ "<br>" +"Votre imc est de "+" "+ imc + "<br>"+ "Vous êtes de corpulence normale")
+
+        } 
+        else if (imc >= 25 && imc <= 29.9) {
+
+            resul.innerHTML= (prenom +"' " +'<br>'+"Votre poid est de"+ "' "+poid+ "<br>"+"Votre taille est de" +" '"+taille+ "<br>" +"Votre imc est de "+" "+ imc + "<br>"+  "Vous êtes en surpoids")
+
+        } else if (imc >= 30 && imc <= 34.9) {
+
+            resul.innerHTML= (prenom +"' " +'<br>'+"Votre poid est de"+ "' "+poid+ "<br>"+"Votre taille est de" +"' "+taille+ "<br>" +"Votre imc est de "+" "+ imc + "<br>"+  "Vous êtes en obésité modéré ")
+
+        } else if (imc >= 35 && imc <= 39.9) {
+
+            resul.innerHTML= (prenom +"' " +'<br>'+"Votre poid est de"+ " '"+poid+ "<br>"+"Votre taille est de" +"' "+taille+ "<br>" +"Votre imc est de "+" "+ imc + "<br>"+  "Vous êtes en obésité sévère ")
+
+        } else {
+            resul.innerHTML= (prenom +"' " +'<br>'+"Votre poid est de"+ "' "+poid+ "<br>"+"Votre taille est de" +" '"+taille+ "<br>" +"Votre imc est de "+" "+ imc + "<br>"+  "Vous êtes en obésité morbide ")
+        }
     }
+}
+
+
     
-    }
+    
