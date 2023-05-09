724e85e66331f07a58a5f87f0cb4f0cc4f47106f
Séverine philippe <severine.philippe27@gmail.com>
2023-05-09 19:17:33 +0200

    finir ecf3

--- back end/ecf3/requete.sql
+++
@@ -1,9 +0,0 @@
-select sum(Pu_HT*quantite),nom_four
-from commande as a join saisie as s on c.num_com=s.num_com
-join produit as p on p.num_pro=s.num_pro
-join fournisseur on f.ref_four=p.ref_four
-group by nom_four;
-
-
-select num_pro,desi_pro,quantite,date_com,nom_four
-from commande
